cdf3ac9dccd8ce2e57a5c303a29f219bb985c415
Aleexy <lixingye1996@gmail.com>
2020-05-19 01:16:50 -0400

    v4

--- BDM_Final_Li_v4.py
+++ BDM_Final_Li_v4.py
@@ -108,14 +108,14 @@ def map_year(row):
 def main(sc):
     #violations = sc.textFile('Parking_Violations_Issued_2020_simplified.csv').mapPartitionsWithIndex(parse_violation)
     violations = sc.textFile('/data/share/bdm/nyc_parking_violation/*.csv', use_unicode=True).mapPartitionsWithIndex(parse_violation)
-    violations = violations.map(lambda x: (x[3], (x[0], x[1], x[2], x[4], x[5])))
+    violations = violations.mapPartitions(lambda x: (x[3], (x[0], x[1], x[2], x[4], x[5])))
 
     #centerline = sc.textFile('nyc_cscl.csv').mapPartitionsWithIndex(parseCL)
     centerline = sc.textFile('/data/share/bdm/nyc_cscl.csv', use_unicode=True).mapPartitionsWithIndex(parseCL)
-    centerline = centerline.map(lambda x: (x[3], (x[0], x[1], x[2], x[4], x[5], x[6], x[7], x[8], x[9], x[10], x[11])))
+    centerline = centerline.mapPartitions(lambda x: (x[3], (x[0], x[1], x[2], x[4], x[5], x[6], x[7], x[8], x[9], x[10], x[11])))
 
     joined = centerline.join(violations).filter(lambda x: (x[1][1][0]!='' and (x[1][0][1] == x[1][1][2] or x[1][0][2] == x[1][1][2])))
-    joined = joined.map(lambda x: ((x[1][0][0], x[1][1][0]),
+    joined = joined.mapPartitions(lambda x: ((x[1][0][0], x[1][1][0]),
                                (x[1][1][1], x[1][0][3], x[1][0][4], x[1][0][5],
                                 x[1][0][6], x[1][0][7], x[1][0][8], x[1][0][9],
                                 x[1][0][10], x[1][1][3], x[1][1][4])))
@@ -124,23 +124,23 @@ def main(sc):
     right = joined.filter(lambda x: x[0][1].isdecimal() and int(x[0][1])%2==0)
 
     filtered_l = left.filter(filter_left).filter(filter_left_HN)
-    filtered_l = filtered_l.map(lambda x: ((x[0][0], x[1][10], x[1][9]), (x[1][0], x[1][2], x[1][4])))
+    filtered_l = filtered_l.mapPartitions(lambda x: ((x[0][0], x[1][10], x[1][9]), (x[1][0], x[1][2], x[1][4])))
 
     filtered_r = right.filter(filter_right).filter(filter_right_HN)
-    filtered_r = filtered_r.map(lambda x: ((x[0][0], x[1][10], x[1][9]), (x[1][0], x[1][6], x[1][8])))
+    filtered_r = filtered_r.mapPartitions(lambda x: ((x[0][0], x[1][10], x[1][9]), (x[1][0], x[1][6], x[1][8])))
 
-    data_l = filtered_l.filter(filter_c).map(lambda x: ((x[0][0], x[0][1]), x[0][2])).distinct().map(lambda x: ((x[0][0]),(x[1])))
-    data_r = filtered_r.filter(filter_c).map(lambda x: ((x[0][0], x[0][1]), x[0][2])).distinct().map(lambda x: ((x[0][0]),(x[1])))
+    data_l = filtered_l.filter(filter_c).mapPartitions(lambda x: ((x[0][0], x[0][1]), x[0][2])).distinct().mapPartitions(lambda x: ((x[0][0]),(x[1])))
+    data_r = filtered_r.filter(filter_c).mapPartitions(lambda x: ((x[0][0], x[0][1]), x[0][2])).distinct().mapPartitions(lambda x: ((x[0][0]),(x[1])))
     data = data_l.union(data_r)
 
-    count = data.map(map_year)
-    allID = centerline.map(lambda x: (x[1][0], (0, 0, 0, 0, 0))).union(count).reduceByKey(lambda x, y: (x[0]+y[0], x[1]+y[1], x[2]+y[2], x[3]+y[3], x[4]+y[4]))
+    count = data.mapPartitions(map_year)
+    allID = centerline.mapPartitions(lambda x: (x[1][0], (0, 0, 0, 0, 0))).union(count).reduceByKey(lambda x, y: (x[0]+y[0], x[1]+y[1], x[2]+y[2], x[3]+y[3], x[4]+y[4]))
 
     diff_x = [-2, -1, 0, 1, 2]
 
-    result = allID.map(lambda x: (x[0], x[1][0], x[1][1], x[1][2], x[1][3], x[1][4], round(sm.OLS([x[1][0], x[1][1], x[1][2], x[1][3], x[1][4]], diff_x).fit().params[0], 2)))
+    result = allID.mapPartitions(lambda x: (x[0], x[1][0], x[1][1], x[1][2], x[1][3], x[1][4], round(sm.OLS([x[1][0], x[1][1], x[1][2], x[1][3], x[1][4]], diff_x).fit().params[0], 2)))
     #result.take(5)
-    return result.map(writeToCSV).saveAsTextFile(sys.argv[1])
+    return result.mapPartitions(writeToCSV).saveAsTextFile(sys.argv[1])
 
 if __name__=="__main__":
     sc = SparkContext()
