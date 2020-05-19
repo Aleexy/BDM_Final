2e15dccf5c145bf8edb23c84ecf67c69abc02aa6
Aleexy <lixingye1996@gmail.com>
2020-05-19 02:08:39 -0400

    v4

--- BDM_Final_Li_v4.py
+++ BDM_Final_Li_v4.py
@@ -114,7 +114,8 @@ def main(sc):
     centerline = sc.textFile('/data/share/bdm/nyc_cscl.csv', use_unicode=True).mapPartitionsWithIndex(parseCL)
     centerline = centerline.map(lambda x: (x[3], (x[0], x[1], x[2], x[4], x[5], x[6], x[7], x[8], x[9], x[10], x[11])))
 
-    joined = centerline.join(violations).filter(lambda x: (x[1][1][0]!='' and (x[1][0][1] == x[1][1][2] or x[1][0][2] == x[1][1][2])))
+    joined = centerline.join(violations)
+    #.filter(lambda x: (x[1][1][0]!='' and (x[1][0][1] == x[1][1][2] or x[1][0][2] == x[1][1][2])))
     '''joined = joined.map(lambda x: ((x[1][0][0], x[1][1][0]),
                                (x[1][1][1], x[1][0][3], x[1][0][4], x[1][0][5],
                                 x[1][0][6], x[1][0][7], x[1][0][8], x[1][0][9],
